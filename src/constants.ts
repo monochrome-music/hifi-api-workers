d8da86281405c62b11ab833cc61e66e21b9b0d37
edidealt <eduardgit@prigoana.com>
2026-03-25 22:31:06 +0200

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
-export const API_VERSION = '1.0'
-export const REPO_URL = 'https://github.com/ap5z/hifi-api-workers'
+export const API_VERSION = '2.7'
+export const REPO_URL = 'https://github.com/monochrome-music/hifi-api-workers'
 export const DEFAULT_COUNTRY_CODE = 'US'
 export const DEFAULT_TRACK_FORMATS = ['HEAACV1', 'AACLC', 'FLAC', 'FLAC_HIRES', 'EAC3_JOC']
 export const ARTIST_TRACK_CONCURRENCY = 20
